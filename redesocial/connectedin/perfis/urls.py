b1cc1d9fdedfeceabbf82bf62fc54dbce1434d2c
felipelhps11 <felipealvess_hpg_@hotmail.comgit config --global user.name F3lipefifa14>
2020-03-10 16:45:24 -0300

    v.0.0.2-F-develop

--- redesocial/connectedin/perfis/urls.py
+++ redesocial/connectedin/perfis/urls.py
@@ -5,5 +5,7 @@ from perfis import views
 urlpatterns = [
     path('' , views.index , name = 'index'),
     re_path(r'^perfis/(?P<perfil_id>\d+)$', views.exibir , name = 'exibir'),
-    re_path(r'^perfis/(?P<perfil_id>\d+)/convidar$' , views.convidar , name= 'convidar')
+    re_path(r'^perfis/(?P<perfil_id>\d+)/convidar$' , views.convidar , name= 'convidar'),
+    re_path(r'^perfis/(?P<convite_id>\d+)/aceitar$' , views.aceitar , name= 'aceitar')
+
 ]
