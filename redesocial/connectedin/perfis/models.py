3753fc7ee731eabf9bdba0695bfa042f915df231
felipelhps11 <felipealvess_hpg_@hotmail.comgit config --global user.name F3lipefifa14>
2020-03-13 16:57:46 -0300

    v.0.0.5-F-develop

--- redesocial/connectedin/perfis/models.py
+++ redesocial/connectedin/perfis/models.py
@@ -6,8 +6,8 @@ class Perfil(models.Model):
     telefone = models.CharField(max_length = 15 , null = False)
     nome_empresa = models.CharField(max_length = 255 , null = False)
     contatos = models.ManyToManyField('self')
-    usuarios = models.OneToOneField(User,on_delete=models.PROTECT , related_name = "perfil")
-
+    usuarios = models.OneToOneField(User, on_delete=models.PROTECT , related_name = "perfil")
+    
     @property
     def email(self):
         return self.usuario.email
--- redesocial/connectedin/usuarios/views.py
+++ redesocial/connectedin/usuarios/views.py
@@ -3,7 +3,6 @@ from django.views.generic.base import View
 from usuarios.forms import RegistrarUsuarioForm
 from django.contrib.auth.models import User
 
-
 class RegistrarUsuarioView(View):
     template_name = "registrar.html"
     def get(self , request):
@@ -13,14 +12,6 @@ class RegistrarUsuarioView(View):
         form = RegistrarUsuarioForm(request.POST)
         if form.is_valid():
             dados_form = form.data
-            usuario = User.objects.create_user(dados_form['nome'] , dados_form['email'], dados_form['senha'])
-
-            perfil = Perfil(nome = dados_form['nome'],
-                            email=dados_form['email'],
-                            senha=dados_form['senha'],
-                            telefone=dados_form['telefone'],
-                            nome_empresa=dados_form['nome_empresa'],
-                            usuario=usuario)
-            perfil.save()
+            usuario = User.objects.create_user(dados_form['nome'], dados_form['email'],dados_form['senha'])
             return redirect('index')
-        return render(request , self.template_name , {'form' : form })
+        return render(request , self.template_name,{'form' : form })
